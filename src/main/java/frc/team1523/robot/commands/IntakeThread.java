f6e3ef10c5e03300db9cd7a4ecb0e99fb7756ec3
Noskcaj <kcajdev@gmail.com>
2018-02-10 11:44:59 -0500

    Formatting

--- src/main/java/frc/team1523/robot/commands/IntakeThread.java
+++ src/main/java/frc/team1523/robot/commands/IntakeThread.java
@@ -4,7 +4,7 @@ import frc.team1523.robot.Robot;
 
 import java.util.Timer;
 
-public class IntakeThread extends Thread{
+public class IntakeThread extends Thread {
 
     public int delay = 2;
 
@@ -17,22 +17,22 @@ public class IntakeThread extends Thread{
      */
 
     @Override
-   public void run(){
+    public void run() {
         Robot.intaker.activate();
         if (Robot.intaker.rightSwitch.get() && Robot.intaker.leftSwitch.get()) {
             Robot.intaker.startMotor();
             doDelay();
             Robot.intaker.cleanUp();
         }
-   }
+    }
 
-   public void doDelay(){
-       try{
-           Thread.sleep(delay);
-       }catch(Exception e){
-           e.printStackTrace();
-       }
-   }
+    public void doDelay() {
+        try {
+            Thread.sleep(delay);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
