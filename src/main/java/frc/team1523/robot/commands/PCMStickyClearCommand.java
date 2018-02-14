1c6c7d97b35469f95a0f03899260058c7112abc1
Noskcaj <kcajdev@gmail.com>
2018-02-14 18:40:37 -0500

    Fixed command

--- src/main/java/frc/team1523/robot/commands/PCMStickyClearCommand.java
+++ src/main/java/frc/team1523/robot/commands/PCMStickyClearCommand.java
@@ -1,11 +1,16 @@
 package frc.team1523.robot.commands;
 
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.team1523.robot.Robot;
 
 public class PCMStickyClearCommand extends InstantCommand {
     public PCMStickyClearCommand() {
-        super("Clear PCM Sticky Faults");
+        super("Clear fault");
+    }
+
+    @Override
+    public synchronized void start() {
         Robot.compressor.clearAllPCMStickyFaults();
     }
 }
