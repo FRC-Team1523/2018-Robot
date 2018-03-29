c084cb1ed09140566ee69ab4d4d21472de002f74
Noskcaj <kcajdev@gmail.com>
2018-03-28 21:08:11 -0400

    Add pid lifter Add side detection wrapper

---
+++ src/main/java/frc/team1523/robot/components/AutoSideWrapper.java
@@ -0,0 +1,34 @@
+package frc.team1523.robot.components;
+
+import edu.wpi.first.wpilibj.command.Command;
+import openrio.powerup.MatchData;
+
+public class AutoSideWrapper extends Command {
+    private Command leftCommand;
+    private Command rightCommand;
+    private Command activeCommand;
+    private MatchData.GameFeature targetFeature;
+
+    public AutoSideWrapper(Command left, Command right, MatchData.GameFeature feature) {
+        leftCommand = left;
+        rightCommand = right;
+        targetFeature = feature;
+    }
+
+    // Or start()
+    @Override
+    protected void initialize() {
+        MatchData.OwnedSide side = MatchData.getOwnedSide(targetFeature);
+        if (side == MatchData.OwnedSide.LEFT) {
+            activeCommand = leftCommand;
+        } else if (side == MatchData.OwnedSide.RIGHT) {
+            activeCommand = rightCommand;
+        }
+        activeCommand.start();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return activeCommand.isCompleted();
+    }
+}
